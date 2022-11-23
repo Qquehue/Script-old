6ed7a3e8a228d46a016dccba2d250b9b9648344f
Qquehue <alejandro.qquehue@sptech.school>
2022-11-22 21:33:57 -0300

    ajustes again

--- maq.sh
+++ maq.sh
@@ -9,6 +9,7 @@ sleep 2
 echo "Digite sua senha:"
 sudo passwd ubuntu
 fi
+sudo su
 echo "Vamos atualizar sua maquina, se aparecer uma tela roxa selecione os espacos utilizando as setas para direcionamento e espaco para marcar (selecione todos os campos), aperte enter para confirmar)"
 sleep 3
 sudo apt update && apt upgrade -y
@@ -36,34 +37,6 @@ docker --version
 if [ $? -eq 0 ];
 then
 echo "docker instalado"
-echo "tem o banco de dados ctc criado? (s/n)"
-read inst
-if [ \"$inst\" == \"n\" ];
-then
-sudo systemctl start Docker
-sudo systemctl enable Docker
-cd ~/Instalador-primario-CTC/bd-mysql
-sudo docker-compose up -d 
-
-sudo docker exec -it $(sudo docker ps -aqf "name=ContainerBD") mysql -u root -p -B -N -e "
-
-USE ctc;
-
-CREATE TABLE Linha ( idLinha INT PRIMARY KEY AUTO_INCREMENT,nomeLinha VARCHAR(45),situacaoLinha CHAR(10));
-
-CREATE TABLE Estacao(idEstacao INT PRIMARY KEY AUTO_INCREMENT,nomeEstacao VARCHAR(45),situacaoEstacao CHAR(10),fkLinha INT,FOREIGN KEY (fkLinha) REFERENCES Linha(idLinha));
-
-CREATE TABLE Maquina(idMaquina INT PRIMARY KEY AUTO_INCREMENT,modeloCPU VARCHAR(45),totalMemoria DOUBLE,totalDisco DOUBLE,dataCadastro DATETIME,fkEstacao INT,FOREIGN KEY (fkEstacao) REFERENCES Estacao(idEstacao));
-
-CREATE TABLE UsoMaquina(idUso INT PRIMARY KEY AUTO_INCREMENT,temperaturaCPU DOUBLE,usoCPU DOUBLE,usoMemoria DOUBLE,upTime DATETIME,fkMaquina INT,FOREIGN KEY (fkMaquina) REFERENCES Maquina(idMaquina));
-
-CREATE TABLE Cargo(idCargo INT PRIMARY KEY AUTO_INCREMENT,nomeCargo VARCHAR(45));
-
-CREATE TABLE Funcionario(idFuncionario INT PRIMARY KEY AUTO_INCREMENT,nomeFuncionario VARCHAR(45),CPF CHAR(11),telefone CHAR(15),email VARCHAR(45),senha VARCHAR(45),fkLinha INT,FOREIGN KEY (fkLinha) REFERENCES Linha(idLinha),fkCargo INT,FOREIGN KEY (fkCargo) REFERENCES Cargo(idCargo));"
-
-exit
-exit
-
 fi
 else
 echo "docker nao instalado"
@@ -98,4 +71,6 @@ fi
 cd arquivos-sh
 chmod 777 tJava.sh
 chmod 777 tNode.sh
+echo "Voce esta aqui:"
+pwd
 echo "Digite o proximo script para execucao da aplicacao (bash tJava.sh)"
