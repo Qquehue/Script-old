7982ad2c4a6af24a4a2a56e73685791f1f2f4b68
Qquehue <alejandro.qquehue@sptech.school>
2022-11-22 21:53:48 -0300

    again

--- maq.sh
+++ maq.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-sudo su
 echo "Vamos atualizar sua maquina, se aparecer uma tela roxa selecione os espacos utilizando as setas para direcionamento e espaco para marcar (selecione todos os campos), aperte enter para confirmar)"
-apt update && apt upgrade -y
-apt-get install xrdp lxde-core lxde tigervnc-standalone-server -y
+sudo apt update && apt upgrade -y
+sudo apt-get install xrdp lxde-core lxde tigervnc-standalone-server -y
 #//////////////////////////JAVA/////////////////////////////////////
 VERSION="$(java -version 2>&1 | grep version | cut -d'"' -f2)"
 if [ "${VERSION}" ];
@@ -16,20 +15,20 @@ echo "gostaria de instalar o java? (s/n)"
 read inst
 if [ \"$inst\" == \"s\" ];
 then
-apt install default-jre -y
+sudo apt install default-jre -y
 fi
 fi
 
-git clone https://github.com/Qquehue/arquivos-sh.git
+git clone git@github.com:Qquehue/arquivos-sh.git
 #////////////////////////////DOCKER//////////////////////////////////
 echo "instalando docker..."
 
-apt install docker.io -y
-  systemctl start docker
-  systemctl enable docker
-  docker pull mysql:5.7
-  docker run -d -p 3306:3306 --name ctc -e "MYSQL_DATABASE=ctc" -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:5.7
-  docker exec -it ctc bash mysql -u root -p -B -N -e"
+sudo apt install docker.io -y
+  sudo systemctl start docker
+  sudo systemctl enable docker
+  sudo docker pull mysql:5.7
+  sudo docker run -d -p 3306:3306 --name ctc -e "MYSQL_DATABASE=ctc" -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:5.7
+  sudo docker exec -it ctc bash mysql -u root -p -B -N -e"
 
 CREATE DATABASE ctc;
 
@@ -51,8 +50,8 @@ exit
 exit
 fi
 cd arquivos-sh
-chmod 777 tJava.sh
-chmod 777 tNode.sh
+sudo chmod 777 tJava.sh
+sudo chmod 777 tNode.sh
 echo "Voce esta aqui:"
 pwd
 echo "Digite o proximo script para execucao da aplicacao (bash tJava.sh)"
